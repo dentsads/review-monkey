8522d14c12013071ad6030041484b240048d4d99
Dimitrios Dentsas <d.dentsas@googlemail.com>
2015-05-02 14:57:39 +0200

    Added jsonschema npm package, started implementation on Review Request Validation

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+// Persistent datastore with automatic loading
+var Datastore = require('nedb');
+// Express web framework
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var Validator = require('jsonschema').Validator;
+
+var app = express();
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -11,90 +16,135 @@ app.use(bodyParser.json());
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+var dbReview = new Datastore({ filename: './.datastore/reviews.db', autoload: true });
+var dbComments = new Datastore({ filename: './.datastore/comments.db', autoload: true });
+var dbUsers = new Datastore({ filename: './.datastore/users.db', autoload: true });
 
-// Persistent datastore with automatic loading
-var Datastore = require('nedb')
-  , db = new Datastore({ filename: './.datastore/datafile.db', autoload: true });
+var ReviewValidator = (function () {
+    var constr = function () {
+        // the constructor
+    };
 
+    constr.isValidReviewRequest = function (req, res) {
+      if (!req.is('application/json')) {
+        res.status(406).send(_createError(1,"The content-type header must be set to application/json")).end();
+        return false;
+      }
+      if (!_isValidReviewObject(req.body)) {
+        res.status(406).send(_createError(2,"The review message structure in the request body is invalid!")).end();
+        return false;
+      }
+    };
+
+    this._isValidReviewObject = function (review) {
+      if (!review) return false;
+      if (!review.message) return false;
+      if (!review.creationDate) return false;
+      if (!review.modificationDate) return false;
+
+      return true;
+    };
+
+    this._createError = function (code, message) {
+      return {error: {
+        'message': message,
+        'code': code
+      }};
+    };
+
+    return constr;
+})();
+
+var ReviewService = (function () {
+    var constr = function () {
+        // the constructor
+    };
+
+    constr.getReview = function () {
+      return function (req, res) {
+        dbReview.find({ _id : req.params.review_id}, function(err, review) {
+          if (err) res.send(err);
+          res.json(review);
+        });
+      };
+    };
+
+    constr.updateReview = function () {
+      return function(req, res) {
+        if (!ReviewValidator.isValidReviewRequest(req, res)) return;
+
+        dbReview.findOne({ _id : req.params.review_id }, function (err, review) {
+      			if (err) res.send(err);
+      			review = req.body;
+            dbReview.update({ _id : req.params.review_id }, req.body, {}, function (err, numReplaced) {
+      			     if (err) res.send(err);
+      			  res.json(review);
+      			});
+  	     });
+      };
+    };
+
+    constr.deleteReview = function () {
+      return function(req, res) {
+        dbReview.remove({ _id : req.params.review_id }, {}, function (err, numRemoved) {
+  			  if (err) res.send(err);
+  			    res.json({ message: 'Review successfully deleted!', 'review': JSON.stringify(review) });
+  		  });
+  	   };
+    };
+
+    constr.getAllReviews = function () {
+      return function(req, res) {
+        dbReview.find({}, function (err, reviews) {
+    			if (err) res.send(err);
+    			res.json(reviews);
+    		});
+      };
+    };
+
+    constr.createReview = function () {
+      return function(req, res) {
+        if (!ReviewValidator.isValidReviewRequest(req, res)) return;
+
+        dbReview.insert(req.body, function (err, review) {
+      		if (err) res.send(err);
+      		res.json({ message: 'Review successfully created!', 'review': JSON.stringify(review) });
+	      });
+      };
+    };
+
+    return constr;
+})();
+
+var CommentService = (function () {
+    var constr = function () {
+        // the constructor
+    };
+
+    constr.getReview = function () {
+      return function (req, res) {
+        dbComments.find({ _id : req.params.review_id}, function(err, review) {
+          if (err) res.send(err);
+          res.json(review);
+        });
+      };
+    };
+
+    return constr;
+})();
 
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+   res.json({ message: 'hooray! welcome to our api!' });
 });
 
 router.route('/reviews')
-
-    // create a review
-    .post(function(req, res) {
-
-        var review = { hello: 'world'
-               , n: 5
-               , today: new Date()
-               , nedbIsAwesome: true
-               , notthere: null
-               , notToBeSaved: undefined  // Will not be saved
-               , fruits: [ 'apple', 'orange', 'pear' ]
-               , infos: { name: 'nedb' }
-               };
-
-        db.insert(review, function (err, review) {
-      		if (err)
-    	        	res.send(err);
-
-      		res.json({ message: 'Review created!' });
-
-      		console.log('inserted document' + review.today);
-	      });
-
-    })
-
-    .get(function(req, res) {
-  		db.find({}, function (err, reviews) {
-  			if (err)
-  		        	res.send(err);
-
-  			res.json(reviews);
-
-  		  // docs is an array containing documents Mars, Earth, Jupiter
-  		  // If no document is found, docs is equal to []
-  		  console.log('found document: ' + reviews[0].fruits);
-  		});
-    });
+    .post(ReviewService.createReview())
+    .get(ReviewService.getAllReviews());
 
 router.route('/reviews/:review_id')
-
-    .get(function(req, res) {
-        db.find({ _id : req.params.review_id}, function(err, review) {
-            if (err)
-                res.send(err);
-            res.json(review);
-        });
-    })
-
-    .put(function(req, res) {
-    	 db.findOne({ _id : req.params.review_id }, function (err, review) {
-    			if (err)
-    		        	res.send(err);
-    			review = req.body;
-    			db.update({ _id : req.params.review_id }, req.body, {}, function (err, numReplaced) {
-    			     if (err)
-    				res.send(err);
-
-    			  res.json(review);
-    			});
-
-	     });
-
-    })
-
-    .delete(function(req, res) {
-
-			db.remove({ _id : req.params.review_id }, {}, function (err, numRemoved) {
-			  if (err)
-				res.send(err);
-
-			    res.json({ message: 'Successfully deleted' });
-			});
-	});
+    .get(ReviewService.getReview())
+    .put(ReviewService.updateReview())
+    .delete(ReviewService.deleteReview());
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
@@ -117,5 +167,4 @@ var server = app.listen(3000, function () {
   var port = server.address().port;
 
   console.log('Review Monkey listening at http://%s:%s', host, port);
-
 });
