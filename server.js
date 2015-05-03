3d8455036a20d86d5374c6cadd6b92632d91237e
Dimitrios Dentsas <d.dentsas@googlemail.com>
2015-05-03 18:39:11 +0200

    Refactored client side GUI Logic (e.g. ReviewInterfaceClient)

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var Datastore = require('nedb');
 var express = require('express');
 var bodyParser = require('body-parser');
 var Validator = require('jsonschema').Validator;
+var http = require('http');
 var fs = require("fs");
 
 var REVIEW_SCHEMA  = JSON.parse(fs.readFileSync("./lib/model/reviewSchema.json", "utf8"));
@@ -21,7 +22,7 @@ app.use(bodyParser.json());
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-var dbReview = new Datastore({ filename: './.datastore/reviews.db', autoload: true });
+var dbReviews = new Datastore({ filename: './.datastore/reviews.db', autoload: true });
 var dbComments = new Datastore({ filename: './.datastore/comments.db', autoload: true });
 var dbUsers = new Datastore({ filename: './.datastore/users.db', autoload: true });
 
@@ -93,6 +94,122 @@ var ReviewValidator = (function () {
     return constr;
 })();
 
+var CommentValidator = (function () {
+    var constr = function () {
+        // the constructor
+    };
+
+    constr.isValidCommentRequest = function (req, res) {
+      if (!_isHttpRequestValid(req, res)) return false;
+      if (!_isSchematicallyValid(req, res)) return false;
+      //if (!_isSemanticallyValid(req, res)) return false;
+
+      // If this point is reached, then the request seems to be valid
+      return true;
+    };
+
+    constr.isSemanticallyValid = function (req, res, callback) {
+      var review = req.body;
+      console.log("Got response: " + review.author.refId);
+      if (review.author)
+      dbUsers.find({ _id : review.author.refId}, function(err, user) {
+        return callback(err, user);
+      });
+      /*
+      http.get(review.author.refId, function(authorRes) {
+        console.log("Got response: " + JSON.stringify(authorRes.body));
+        if (authorRes.body == 'undefined') {
+          res.status(406).send(ErrorHandler.createError(3,"Could not find user " + review.author.refId)).end();
+          return false;
+        }
+      }).on('error', function(e) {
+        res.status(406).send(ErrorHandler.createError(3,"Could not find user " + review.author)).end();
+        return false;
+        console.log("Got error: " + e.message);
+      });
+      */
+    };
+
+    constr._isSemanticallyValidCallback = function (res) {
+      return function (err, user) {
+        if (err || user.length == 0) {
+          console.log("inside error");
+          res.status(406).send(ErrorHandler.createError(3,"Could not find user " + user)).end();
+          return false;
+        }
+
+        return true;
+      };
+    };
+
+    this._isSchematicallyValid = function (req, res) {
+      // Validate comment json message against schema in commentSchema.json
+      var validationResult = _validateCommentObject(req.body);
+      if (validationResult.errors.length !== 0) {
+        var errorArray = [];
+        for (var i = 0 ; i < validationResult.errors.length; i++) {
+          var error = validationResult.errors[i];
+          errorArray.push({'message': error.property + " " + error.message, 'code': 2});
+        }
+        res.status(406).send(ErrorHandler.createErrors(errorArray)).end();
+        return false;
+      }
+
+      return true;
+    };
+
+    this._isHttpRequestValid = function (req, res) {
+      // Check if content-type is application/json*
+      if (!req.is('application/json')) {
+        res.status(406).send(ErrorHandler.createError(1,"The content-type header must be set to application/json")).end();
+        return false;
+      }
+
+      return true;
+    };
+
+    this._validateCommentObject = function (comment) {
+      return validator.validate(comment, COMMENT_SCHEMA);
+    };
+
+    return constr;
+})();
+
+var UserValidator = (function () {
+    var constr = function () {
+        // the constructor
+    };
+
+    constr.isValidUserRequest = function (req, res) {
+      // Check if content-type is application/json*
+      if (!req.is('application/json')) {
+        res.status(406).send(ErrorHandler.createError(1,"The content-type header must be set to application/json")).end();
+        return false;
+      }
+
+      // Validate user json message against schema in commentSchema.json
+      var validationResult = _validateUserObject(req.body);
+      if (validationResult.errors.length !== 0) {
+        var errorArray = [];
+        for (var i = 0 ; i < validationResult.errors.length; i++) {
+          var error = validationResult.errors[i];
+          errorArray.push({'message': error.property + " " + error.message, 'code': 2});
+        }
+        res.status(406).send(ErrorHandler.createErrors(errorArray)).end();
+        return false;
+      }
+
+      // If this point is reached, then the request seems to be valid
+      return true;
+    };
+
+    this._validateUserObject = function (user) {
+      return validator.validate(user, USER_SCHEMA);
+    };
+
+    return constr;
+})();
+
 var ReviewService = (function () {
     var constr = function () {
         // the constructor
@@ -100,7 +217,7 @@ var ReviewService = (function () {
 
     constr.getReview = function () {
       return function (req, res) {
-        dbReview.find({ _id : req.params.review_id}, function(err, review) {
+        dbReviews.find({ _id : req.params.review_id}, function(err, review) {
           if (err) res.send(err);
           res.json(review);
         });
@@ -111,10 +228,13 @@ var ReviewService = (function () {
       return function(req, res) {
         if (!ReviewValidator.isValidReviewRequest(req, res)) return;
 
-        dbReview.findOne({ _id : req.params.review_id }, function (err, review) {
+        var currentDatetime = new Date().toString();
+        req.body.modificationDate = currentDatetime;
+
+        dbReviews.findOne({ _id : req.params.review_id }, function (err, review) {
       			if (err) res.send(err);
       			review = req.body;
-            dbReview.update({ _id : req.params.review_id }, req.body, {}, function (err, numReplaced) {
+            dbReviews.update({ _id : req.params.review_id }, req.body, {}, function (err, numReplaced) {
       			     if (err) res.send(err);
       			  res.json(review);
       			});
@@ -124,16 +244,16 @@ var ReviewService = (function () {
 
     constr.deleteReview = function () {
       return function(req, res) {
-        dbReview.remove({ _id : req.params.review_id }, {}, function (err, numRemoved) {
+        dbReviews.remove({ _id : req.params.review_id }, {}, function (err, numRemoved) {
   			  if (err) res.send(err);
-  			    res.json({ message: 'Review successfully deleted!', 'review': JSON.stringify(review) });
+  			    res.json({ message: 'Review successfully deleted!'});
   		  });
   	   };
     };
 
     constr.getAllReviews = function () {
       return function(req, res) {
-        dbReview.find({}, function (err, reviews) {
+        dbReviews.find({}, function (err, reviews) {
     			if (err) res.send(err);
     			res.json(reviews);
     		});
@@ -144,7 +264,11 @@ var ReviewService = (function () {
       return function(req, res) {
         if (!ReviewValidator.isValidReviewRequest(req, res)) return;
 
-        dbReview.insert(req.body, function (err, review) {
+        var currentDatetime = new Date().toString();
+        req.body.creationDate = currentDatetime;
+        req.body.modificationDate = currentDatetime;
+
+        dbReviews.insert(req.body, function (err, review) {
       		if (err) res.send(err);
       		res.json({ message: 'Review successfully created!', 'review': JSON.stringify(review) });
 	      });
@@ -159,14 +283,142 @@ var CommentService = (function () {
         // the constructor
     };
 
-    constr.getReview = function () {
+    constr.getComment = function () {
       return function (req, res) {
-        dbComments.find({ _id : req.params.review_id}, function(err, review) {
+        dbComments.find({ _id : req.params.comment_id}, function(err, comment) {
           if (err) res.send(err);
-          res.json(review);
+          res.json(comment);
         });
       };
     };
+
+    constr.updateComment = function () {
+      return function(req, res) {
+        if (!CommentValidator.isValidCommentRequest(req, res)) return;
+
+        var currentDatetime = new Date().toString();
+        req.body.modificationDate = currentDatetime;
+
+        dbComments.findOne({ _id : req.params.comment_id }, function (err, comment) {
+            if (err) res.send(err);
+            comment = req.body;
+            dbComments.update({ _id : req.params.comment_id }, req.body, {}, function (err, numReplaced) {
+                 if (err) res.send(err);
+              res.json(comment);
+            });
+         });
+      };
+    };
+
+    constr.deleteComment = function () {
+      return function(req, res) {
+        dbComments.remove({ _id : req.params.comment_id }, {}, function (err, numRemoved) {
+  			  if (err) res.send(err);
+  			    res.json({ message: 'Comment successfully deleted!' });
+  		  });
+  	   };
+    };
+
+    constr.getAllComments = function () {
+      return function(req, res) {
+        dbComments.find({}, function (err, comments) {
+          if (err) res.send(err);
+          res.json(comments);
+        });
+      };
+    };
+
+    constr.createComment = function () {
+      return function(req, res) {
+        if (!CommentValidator.isValidCommentRequest(req, res)) return;
+        //if (!CommentValidator.isSemanticallyValid(req, res,
+        //     CommentValidator._isSemanticallyValidCallback(res))) return;
+
+        var currentDatetime = new Date().toString();
+        req.body.creationDate = currentDatetime;
+        req.body.modificationDate = currentDatetime;
+
+        dbComments.insert(req.body, function (err, comment) {
+      		if (err) res.send(err);
+      		res.json({ message: 'Comment successfully created!', 'comment': JSON.stringify(comment) });
+	      });
+      };
+    };
+
+    return constr;
+})();
+
+var UserService = (function () {
+    var constr = function () {
+        // the constructor
+    };
+
+    constr.getUser = function () {
+      return function (req, res) {
+        dbUsers.find({ _id : req.params.user_id}, function(err, user) {
+          if (err) res.send(err);
+          res.json(user);
+        });
+      };
+    };
+
+    constr.updateUser = function () {
+      return function(req, res) {
+        if (!UserValidator.isValidUserRequest(req, res)) return;
+
+        dbUsers.findOne({ _id : req.params.user_id }, function (err, user) {
+            if (err) res.send(err);
+            user = req.body;
+            dbUsers.update({ _id : req.params.user_id }, req.body, {}, function (err, numReplaced) {
+                 if (err) res.send(err);
+              res.json(user);
+            });
+         });
+      };
+    };
+
+    constr.deleteUser = function () {
+      return function(req, res) {
+        dbUsers.remove({ _id : req.params.user_id }, {}, function (err, numRemoved) {
+  			  if (err) res.send(err);
+  			    res.json({ message: 'User successfully deleted!' });
+  		  });
+  	   };
+    };
+
+    constr.getAllUsers = function () {
+      return function(req, res) {
+        dbUsers.find({}, function (err, users) {
+          if (err) res.send(err);
+          res.json(users);
+        });
+      };
+    };
+
+    constr.createUser = function () {
+      return function(req, res) {
+        if (!UserValidator.isValidUserRequest(req, res)) return;
+
+        dbUsers.insert(req.body, function (err, user) {
+      		if (err) res.send(err);
+
+          if (user.avatarImage)
+            saveAvatarImage(user.avatarImage, user.id);
+
+      		res.json({ message: 'User successfully created!', 'user': JSON.stringify(user) });
+	      });
+      };
+    };
+
+    this.saveAvatarImage = function (b64Data, userId) {
+      fs.writeFile("./web/dist/img/" + userId + ".png", b64Data, {'encoding': 'base64', 'flags': 'wx'}, function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("The file was saved as " + "./web/dist/img/" + userId + ".png");
+        }
+      });
+    }
 
     return constr;
 })();
@@ -183,6 +435,24 @@ router.route('/reviews/:review_id')
     .get(ReviewService.getReview())
     .put(ReviewService.updateReview())
     .delete(ReviewService.deleteReview());
+
+router.route('/comments')
+    .post(CommentService.createComment())
+    .get(CommentService.getAllComments());
+
+router.route('/comments/:comment_id')
+    .get(CommentService.getComment())
+    .put(CommentService.updateComment())
+    .delete(CommentService.deleteComment());
+
+router.route('/users')
+    .post(UserService.createUser())
+    .get(UserService.getAllUsers());
+
+router.route('/users/:user_id')
+    .get(UserService.getUser())
+    .put(UserService.updateUser())
+    .delete(UserService.deleteUser());
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api/<version>/
--- web/angular/js/controllers.js
+++ web/angular/js/controllers.js
@@ -11,9 +11,101 @@ reviewsApp.controller('ReviewListCtrl', function ($scope, $http) {
 
 reviewsApp.controller('ReviewDetailCtrl', function ($scope, $http, $routeParams) {
   $http.get(apiBasePath + "/reviews/" + $routeParams.review_id).success(function(data) {
-    $scope.reviewDetail = data;
+    var review = data[0];
+    $scope.reviewDetail = review;
+
+    for (var i = 0 ; i < review.changes.length; i++) {
+      var change = review.changes[i];
+
+      ReviewInterfaceClient.createDiffBoxWithInlineComments(i, StringUtils.b64_to_utf8(change.udiff));
+    }
+
   });
 
   $scope.id = $routeParams.review_id;
+  $scope.getReviewStatusClass = ReviewInterfaceClient.getReviewStatusClass;
 
 });
+
+var StringUtils = (function () {
+    var constr = function () {
+        // the constructor
+    };
+
+    constr.utf8_to_b64 = function (str) {
+        return window.btoa(unescape(encodeURIComponent(str)));
+    };
+
+    constr.b64_to_utf8 = function (str) {
+        return decodeURIComponent(escape(window.atob(str)));
+    };
+
+    return constr;
+})();
+
+var ReviewInterfaceClient = (function () {
+    var constr = function () {
+        // the constructor
+    };
+
+    constr.getReviewStatusClass = function(reviewStatus) {
+      if (reviewStatus == 'open') return "fa-circle-o";
+      if (reviewStatus == 'rejected') return "fa-times-circle text-red";
+      if (reviewStatus == 'accepted') return "fa-check-circle text-green";
+
+      return "";
+    };
+
+    constr.createDiffBoxWithInlineComments = function (diffId, uDiffString) {
+      var diffArray = uDiffString.split(/\u21B5/g)||[];
+
+      $("#diff-boxes").append(_getDiffBoxString(diffId, diffArray));
+
+      for (var i = 1; i < diffArray.length -3; i++) {
+        $('#diff'+diffId+'-' + i).click(_insertInlineCommentFormAfterLine(diffId, i));
+      }
+    };
+
+    this._insertInlineCommentFormAfterLine = function (diffId, line) {
+      return function () {
+        $('.inline-comment-form').hide();
+        $('.inline-comment-form-textarea').val('');
+        $('.inline-comment-form').insertAfter('#diff'+diffId+'-' + line);
+        $('.inline-comment-form').show();
+      };
+    };
+
+    this._getDiffBoxString = function (diffId, diffArray) {
+      var diffLines = "";
+
+      for (var i = 1; i < diffArray.length -3; i++) {
+        var mode = "";
+        if (diffArray[i+2].startsWith("-"))
+          mode = "1";
+        if (diffArray[i+2].startsWith("+"))
+          mode = "2";
+
+        diffLines += "<div id='diff"+diffId+"-"+i+"' class='diff"+ mode +"'>"+ diffArray[i + 2] + "</div>";
+      }
+
+      return "<div class='row'> \
+              <div class='col-md-12'> \
+              <div class='box'> \
+              <div class='box-header with-border'> \
+              <h3 class='box-title'>"+ diffArray[0] +"</h3> \
+              </div> \
+              <div class='box-body'> \
+              <div class='diff-div'> \
+              <div> \
+              "+diffLines+" \
+              </div> \
+              </div> \
+              </div> \
+              </div> \
+              </div> \
+              </div>";
+
+    };
+
+    return constr;
+})();
